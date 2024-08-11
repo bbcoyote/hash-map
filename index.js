58327b8127c54bb6e6097a44374db85a23c50082
Charles Huffman <huffmancharles14@gmail.com>
2024-08-11 10:14:55 -0500

    add hashing function

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function deleteNode(value) {
     }
 }
 
-function update(value, newValue) {
+function updateNode(value, newValue) {
     let current = this.head;
     while (current) {
         if (current.value === value) {
@@ -50,7 +50,7 @@ function isEmpty() {
     return this.head === null;
 }
 
-function clear() {
+function clearAllNodes() {
     let current = this.head;
     while (current) {
         let nextNode = current.next;
@@ -73,6 +73,23 @@ function getAllValues() { // as an array
 function linkedList() {
     return {
         node: node,
-        appendNode: appendNode
+        appendNode: appendNode,
+        deleteNode: deleteNode,
+        updateNode: updateNode,
+        isEmpty: isEmpty,
+        clearAllNodes: clearAllNodes,
+        getAllValues: getAllValues
     }
 }
+
+function hashKey(key, size) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % size;
+    }
+    return hashCode;
+}
+
+console.log(hashKey("tim", 16)) // returns 4136
